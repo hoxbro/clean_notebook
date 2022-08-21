4d5d60aa5d16ecf2d1363abc231ca8d816b5676a
Simon Høxbro <simon.hansen@me.com>
2022-08-21 11:08:43 +0200

    Added functionality to clean notebook

--- src/clean_notebook/clean_notebook.py
+++ src/clean_notebook/clean_notebook.py
@@ -1,10 +1,44 @@
-def clean_notebooks(arguments: list[str]):
-    for argument in arguments:
-        clean_notebook(argument)
+from __future__ import annotations
+
+import json
+from pathlib import Path
+from typing import Any
 
 
-def clean_notebook(file):
-    print(file)
+def clean_notebook(files: list[str | Path], dryrun: bool = False):
+    for file in files:
+        _clean_single_notebook(file, dryrun)
+
+
+def _clean_single_notebook(file: str | Path, dryrun: bool = False):
+    if not str(file).endswith(".ipynb"):
+        return
+
+    with open(file) as f:
+        nb = json.load(f)
+
+    cleaned = False
+    for cell in nb["cells"]:
+        cleaned |= _update_value(cell, "outputs", [])
+        cleaned |= _update_value(cell, "execution_count", None)
+        cleaned |= _update_value(cell, "metadata", {})
+
+    metadata = {"language_info": {"name": "python", "pygments_lexer": "ipython3"}}
+    cleaned |= _update_value(nb, "metadata", metadata)
+
+    if cleaned:
+        if not dryrun:
+            with open(file, "w") as f:
+                json.dump(nb, f)
+        print(f"Cleaned notebook: {file}")
+
+
+def _update_value(dct: dict[str, Any], key: str, value: Any) -> bool:
+    if key in dct and dct[key] != value:
+        dct[key] = value
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
