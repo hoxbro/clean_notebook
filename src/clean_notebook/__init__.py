5511b15086441ca5eb1f3dacc9e82facd2893227
Simon Høxbro Hansen <simon.hansen@me.com>
2023-10-08 08:45:58 +0200

    Move functions around and make private

--- src/clean_notebook/__init__.py
+++ src/clean_notebook/__init__.py
@@ -2,4 +2,6 @@ from __future__ import annotations
 
 import importlib.metadata
 
+from .clean import clean_notebook, clean_single_notebook  # noqa: F401
+
 __version__ = importlib.metadata.version("clean_notebook")
