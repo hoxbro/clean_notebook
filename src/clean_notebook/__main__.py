81e35aa6b1c027cb26129e333b6d102687612baf
Simon Høxbro Hansen <simon.hansen@me.com>
2023-12-08 17:34:31 +0100

    Fix spelling mistake

--- src/clean_notebook/__main__.py
+++ src/clean_notebook/__main__.py
@@ -36,7 +36,7 @@ def _create_parser() -> argparse.ArgumentParser:
         help="Metadata keys to ignore when cleaning",
     )
     parser.add_argument(
-        "--strip-endling-newline",
+        "--strip-ending-newline",
         "-s",
         dest="strip",
         action="store_true",
