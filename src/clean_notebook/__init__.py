fd07655a88bcc90df4b197b252f413dd5f17ea63
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-03-17 08:33:05 +0100

    Add _version.py

--- src/clean_notebook/__init__.py
+++ src/clean_notebook/__init__.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 
 from .clean import clean_notebook, clean_single_notebook
 
-__version__ = "0.1.15"
+try:
+    from ._version import __version__
+except Exception:
+    __version__ = "0.0.0"
 
 __all__ = ("__version__", "clean_notebook", "clean_single_notebook")
